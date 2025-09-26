ac53721f367ac815b8fcd268498348b574989d5f
kunwookim123 <attluww@naver.com>
2025-09-26 14:11:34 +0900

    rpg_game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from rpg_game.utils.helpers import choose_character         # __init__을 통한 패키지로 묶인 함수 사용
-from rpg_game.battle.battle_manager import BattleManager
+from utils.helpers import choose_character         # helpers 파일에서 choose_character 클래스 import
+from battle.battle_manager import BattleManager    # battle_manager 파일에서 BattleManager 클래스 import
 
 def main():
     print("=== RPG 전투 게임 ===")
@@ -10,17 +10,17 @@ def main():
         player.reset_health()
         enemy.reset_health()
 
-        battle = BattleManager(player, enemy)               # __init__을 통한 패키지로 묶인 함수 사용
+        battle = BattleManager(player, enemy)               # battle_manager 파일에서 BattleManager 클래스 import
         battle.start_battle()
 
         if not player.is_alive():                           # 플레이어 사망
             print("게임 오버! 당신은 패배했습니다.")
-            break                                           # while문 탈출
+            break                                           # 반복문 탈출
 
         again = input("계속 전투를 하시겠습니까? (y/n): ")     # 전투 완료후 플레이어 승리시 실행
         if again.lower() != 'y':
             print("게임을 종료합니다.")                       # 'n' 입력시 실행
             break                                           # 반복문 탈출
 
-if __name__ == "__main__":
+if __name__ == "__main__": # 'main'함수 호출
     main()
